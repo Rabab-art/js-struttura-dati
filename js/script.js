8172d7a27517a1d95ae4fceeefd2049d50168eba
Rabab-art <bouhamla.rabab@gmail.com>
2021-07-27 15:17:30 +0200

    Creazinoe delle 3 carte e inizio creazione del filtro

--- js/script.js
+++ js/script.js
@@ -1,60 +1,140 @@
-const firstCard = {
-    id: 1,
-    name: 'Bloodfire Colosses',
-    launchCost: ['6', 'R', 'R'],
-    combineManaCost: 8,
-    cardType: 'creatore',
-    subType: 'giant',
-    expansion: {
-        reprintId: 9,
-        name: 'Espansione Fighissima',
-        rarity: 'goldenrod',
-        collectionNr: 12,
-        totalCard: 42,
-    },
-    flavorTex: {
-        quote: 'We cannot...',
-        author: 'Orso Bubu',
-    },
-    abilities: [
-        {
-            launchCost: ['R', 'T'],
-            description: 'lorem...',
+const myDeck = [
+    {
+        id: 1,
+        name: 'Bloodfire Colosses',
+        launchCost: ['6', 'R', 'R'],
+        combineManaCost: 8,
+        cardType: 'creatore',
+        subType: 'giant',
+        expansion: {
+            reprintId: 9,
+            name: 'Espansione Fighissima',
+            rarity: 'goldenrod',
+            collectionNr: 12,
+            totalCard: 42,
         },
-        {
-            launchCost: ['w', 'B', 'T'],
-            description: 'lorem...',
-        }
-    ],
-    collectionNr: '12/43', /*lasciamostringa perchè abbiamo deciso di non utilizzarlo per i filtri*/
-    constitution: 13,
-    strength: 13,
-    borderColor: '#000',
-    illustration: {
-        author: {
-            id: 1,
-            name: 'Greg Smith',
+        flavorTex: {
+            quote: 'We cannot...',
+            author: 'Orso Bubu',
         },
-        source: '../img/pic'
-    },
-    backgorund: {
-        color: 'red',
-        source: '..img',
+        abilities: [
+            {
+                launchCost: ['R', 'T'],
+                description: 'lorem...',
+            },
+            {
+                launchCost: ['w', 'B', 'T'],
+                description: 'lorem...',
+            }
+        ],
+        collectionNr: '12/43', /*lasciamostringa perchè abbiamo deciso di non utilizzarlo per i filtri*/
+        constitution: 13,
+        strength: 13,
+        borderColor: '#000',
+        illustration: {
+            author: {
+                id: 1,
+                name: 'Greg Smith',
+            },
+            source: '../img/pic'
+        },
+        backgorund: {
+            color: 'red',
+            source: '..img',
+
+        },
 
     },
+    {
+        id: 2,
+        name: 'Dragon fire',
+        launchCost: ['1', 'G'],
+        combineManaCost: 4,
+        cardType: 'creatore',
+        subType: 'giant',
+        expansion: {
+            reprintId: 9,
+            name: 'Espansione Fighissima',
+            rarity: 'silver',
+            collectionNr: 12,
+            totalCard: 42,
+        },
+        flavorTex: {
+            quote: 'We cannot...',
+            author: 'Dreagone furioso',
+        },
+        abilities: [
 
-};
-console.log(firstCard);
+        ],
+        collectionNr: '12/43', /*lasciamostringa perchè abbiamo deciso di non utilizzarlo per i filtri*/
+        constitution: 13,
+        strength: 13,
+        borderColor: '#000',
+        illustration: {
+            author: {
+                id: 2,
+                name: 'Greg Smith',
+            },
+            source: '../img/pic'
+        },
+        backgorund: {
+            color: 'red',
+            source: '..img',
 
-/*STAMPA IN PAGINA*/
+        },
 
-const cardSection = document.getElementById('cards');
+    },
+    {
+        id: 3,
+        name: 'Big Foot',
+        launchCost: ['4', 'R'],
+        combineManaCost: 8,
+        cardType: 'creatore',
+        subType: 'Grande scimmia',
+        expansion: {
+            reprintId: 9,
+            name: 'Espansione Fighissima',
+            rarity: 'white',
+            collectionNr: 12,
+            totalCard: 42,
+        },
+        flavorTex: {
+            quote: 'We cannot...',
+            author: 'Orso Bubu',
+        },
+        abilities: [
+            {
+                launchCost: ['R', 'T'],
+                description: 'lorem...',
+            },
+            {
+                launchCost: ['w', 'B', 'T'],
+                description: 'lorem...',
+            }
+        ],
+        collectionNr: '12/43', /*lasciamostringa perchè abbiamo deciso di non utilizzarlo per i filtri*/
+        constitution: 13,
+        strength: 13,
+        borderColor: '#000',
+        illustration: {
+            author: {
+                id: 3,
+                name: 'Greg Smith',
+            },
+            source: '../img/pic'
+        },
+        backgorund: {
+            color: 'red',
+            source: '..img',
 
-const cardTemplate = createCardTemplate(firstCard);
+        },
 
-cardSection.innerHTML = cardTemplate;
+    }
+];
 
-function createCardTemplate(card) {
+
+// FUNCTIONS
+const createCardTemplate = (card) => {
     const subType = card.subType ? `- ${card.subType} ` : '';
     let abilitiesContente = '<em> Nessuna Abilità</em>';
     if (card.abilities.length > 0) {
@@ -100,6 +180,26 @@ function createCardTemplate(card) {
     return cardTemplate;
 
 };
+
+
+const renderDeck = (deck, targertEelement) => {
+    let deckTemplate = '';
+    for (let i = 0; i < deck.length; i++) {
+        const currentCard = deck[i];
+        const currentCardTemplate = createCardTemplate(currentCard);
+        deckTemplate += currentCardTemplate;
+    }
+    targertEelement.innerHTML = deckTemplate;
+};
+
+
+
+/*STAMPA IN PAGINA*/
+const cardSection = document.getElementById('cards');
+renderDeck(myDeck, cardSection);
+
+
+
 
 
 
